a9f01157aebbe9c3d25b5c2cd6515b4d43cfee64
Leon Bryan <leonard.bryan.email@gmail.com>
2025-12-30 19:46:59 +0800

    SEO Optimization for Hero Logo in first section

--- app/components/HeroLogoWrapper.tsx
+++ app/components/HeroLogoWrapper.tsx
@@ -10,41 +10,43 @@ const HeroLogoWrapper = () => {
       initial={{
         opacity: 0,
         scale: 0,
-        transition: { duration: 0.2 }
+        transition: { duration: 0.2 },
       }}
       whileInView={{
         opacity: 1,
         scale: 1,
-        transition: { duration: 0.2 }
+        transition: { duration: 0.2 },
       }}
       viewport={{ once: true }}
-
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
     >
       <Image
         src={"/images/logos/erl-the-bird-text.png"}
-        alt="Logo Vertical"
+        alt="Erl, the Bird Logo"
         width={325}
         height={325}
         quality={100}
         className="-rotate-14 lg:hidden md:hidden block"
+        priority
       />
       <Image
         src={"/images/logos/erl-the-bird-text.png"}
-        alt="Logo Vertical"
+        alt="Erl, the Bird Logo"
         width={450}
         height={450}
         quality={100}
         className="-rotate-14 lg:hidden md:block hidden"
+        priority
       />
       <Image
         src={"/images/logos/erl-the-bird-text-x.png"}
-        alt="Logo Horizontal"
+        alt="Erl, the Bird Logo"
         width={1000}
         height={1000}
         quality={100}
         className="-rotate-8 lg:block md:hidden hidden"
+        priority
       />
     </motion.div>
   );
